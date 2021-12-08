602a658b4a5dfc1a1d5302f5ab022270e4bd740f
CameronJones5 <cameron.jones@hgs.hiddengeniusproject.org>
2021-12-08 01:36:08 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+  
+  public class Main {
+    public static void main(String[]  args) {  //method 
+     System.out.println("what is your name?");
+        //type identifier = new type(); 
+       Scanner scanner = new Scanner (System.in);
+       String name = scanner.nextLine();
+ 
+    System.out.println("hello " + name);
+  
+  
+  }
+
+
+  }
